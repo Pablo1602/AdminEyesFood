bf1a8f36f621d23efc248d9d789ff06ee1ecceff
Pablo <pablo.cancino@usach.cl>
2020-08-25 20:42:50 -0400

    Actualizar menu principal para coach

--- application/views/layouts/default_layout.php
+++ application/views/layouts/default_layout.php
@@ -78,7 +78,7 @@
                       <li class="" <?php if ($rol != 1){ echo 'style="display:none;"'; } ?>>
                             <a href="<?php echo base_url();?>Notificaciones"><i class="fa fa-bullhorn"></i> <span>Notificaciones</span></a>
                         </li>
-                        <li class="treeview" <?php if ($rol ==3 or $rol ==4){ echo 'style="display:none;"'; } ?>>
+                        <li class="treeview" <?php if ($rol == 4){ echo 'style="display:none;"'; } ?>>
                             <a href="#">
                                 <i class="fa fa-apple"></i> <span>Alimentos</span>
                                 <span class="pull-right-container">
@@ -114,7 +114,7 @@
                                 <!--<li><a href="<?php echo base_url();?>Peligros/index"><i class="fa fa-exclamation-triangle"></i> <span>Peligros</span></a></li>-->
                             </ul>
                         </li>
-                        <li class="" <?php if ($rol != 3 and $rol!=2){ echo 'style="display:none;"'; } ?>>
+                        <li class="" <?php if ($rol != 3 and $rol != 2){ echo 'style="display:none;"'; } ?>>
                             <a href="<?php echo base_url();?>Consultas"><i class="fa fa-file"></i> <span>Consultas</span></a>
                         </li>
                         <li <?php if ($rol != 1){ echo 'style="display:none;"'; } ?>><a href="<?php echo base_url();?>Expertos/Nutricionistas"><i class="fa fa-user-md"></i> <span>Nutricionistas</span></a></li>
